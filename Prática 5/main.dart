cd69f1f254cef39685594cf1de59b5d958c49b0a
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-07 09:20:39 -0300

    Update main.dart

--- Prática 5/main.dart
+++ Prática 5/main.dart
@@ -1,65 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-        _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+  runApp(
+    Column(
+        textDirection: TextDirection.ltr,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Neste exemplo, crossAxisAligmnment é definido',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'como CorssAxixAlignment.start, para que os ',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'filhos sejam alinhados à esquerda. O mainAxisSize ',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'è definido como MainAxisSize.min, para que a coluna ',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Google Flutter.',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.white,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
+          ),
+        ]),
+  );
 }
+
