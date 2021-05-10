ac0bf0c3d7fbfa5f73ead23c6dc1a4e4e619e6f7
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-10 20:32:44 -0300

    Prática 4

--- Prática 4/main.dart
+++ Prática 4/main.dart
@@ -1,65 +1,39 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-        _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+  runApp(Row(
+    textDirection: TextDirection.ltr,
+    children: [
+      Expanded(
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: Image(
+            image: NetworkImage(
+                'https://github.com/flutter/plugins/blob/master/packages/video_player/video_player/doc/demo_ipod.gif?raw=true',
+            )
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
+      Expanded(
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: Image(
+            image: NetworkImage(
+              'https://picsum.photos/250?image=9',
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: Image(
+            image: NetworkImage(
+                'https://i.picsum.photos/id/9/250/250.jpg?hmac=tqDH5wEWHDN76mBIWEPzg1in6egMl49qZeguSaH9_VI',
+            )
+          ), //TextStyle.
+        ),
+      )
+    ],
+  ));
 }
