82282f9dae76d4f4b8c6179a9958be0eac33aa95
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-12 20:07:50 -0300

    Pratica 7

--- Prática 6/main.dart
+++ Prática 6/main.dart
@@ -1,65 +1,46 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/material.dart'; 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-        _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+var child;
+var list2 = <Widget>[
+        
+         ),
+          Container(
+            width: 250,
+            height: 250,
+            color: Colors.indigo[400],
+          ),
+          Container(
+                 child: Image(
+          image: NetworkImage(
+            'https://media.giphy.com/media/xT0xezQGU5xCDJuCPe/giphy.gif'
+          ),
+             padding: EdgeInsets.all(5.0),
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+            gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+                      Colors.black.withAlpha(0),
+                      Colors.black12,
+                      Colors.black45
+                    ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
+          ),
+          ),
+          
+var children2 = list;
+runApp(Center(      
+     child: SizedBox(
+      width: 250,     
+      height: 250,
+     child: Stack(
+     textDirection: TextDirection.ltr,    
+        children: children2,
+     ),
+     ),
+),
+);   
+
 }
