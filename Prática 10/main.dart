700a4cfc62bea9634b28e06da87ee055777bf4e9
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-13 15:17:01 -0300

    Calculadora de IMC

--- Prática 10/main.dart
+++ Prática 10/main.dart
@@ -1,108 +1,142 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
 class Home extends StatefulWidget {
-  HomeState createState() {
-    return HomeState();
-  }
+  @override
+  _HomeState createState() => _HomeState();
 }
 
-class HomeState extends State<Home> {
-  TextEditingController num01Controller = TextEditingController();
-  TextEditingController num02Controller = TextEditingController();
-  String resp = " ";
+class _HomeState extends State<Home> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  somar() {
-    int num01 = int.parse(this.num01Controller.text);
-    int num02 = int.parse(this.num02Controller.text);
-    int soma = num01 + num02;
-    this.resp = '$num01 + $num02 = $soma';
-    return this.resp;
+  TextEditingController _weightController = TextEditingController();
+  TextEditingController _heightController = TextEditingController();
+  String _result;
+
+  @override
+  void initState() {
+    super.initState();
+    resetFields();
   }
 
-  subitrair() {
-    int num01 = int.parse(this.num01Controller.text);
-    int num02 = int.parse(this.num02Controller.text);
-    int soma = num01 + num02;
-    this.resp = '$num01 - $num02 = $soma';
-    return this.resp;
+  void resetFields() {
+    _weightController.text = '';
+    _heightController.text = '';
+    setState(() {
+      _result = 'Informe os seus dados';
+    });
   }
 
-  multiplicao() {
-    int num01 = int.parse(this.num01Controller.text);
-    int num02 = int.parse(this.num02Controller.text);
-    int soma = num01 + num02;
-    this.resp = '$num01 * $num02 = $soma';
-    return this.resp;
-  }
+  void calculateImc() {
+    double weight = double.parse(_weightController.text);
+    double height = double.parse(_heightController.text) / 100.0;
+    double imc = weight / (height * height);
 
-  divisao() {
-    int num01 = int.parse(this.num01Controller.text);
-    int num02 = int.parse(this.num02Controller.text);
-    int soma = num01 + num02;
-    this.resp = '$num01 / $num02 = $soma';
-    return this.resp;
+    setState(() {
+      _result = "IMC = ${imc.toStringAsPrecision(2)}\n";
+      if (imc < 16)
+        _result += "Magreza grave";
+      else if (imc < 16.0 || imc < 17.0)
+        _result += "Magreza moderada";
+      else if (imc < 17.0 || imc < 18.5)
+        _result += "Magreza leve";
+      else if (imc < 18.5 || imc < 25.0)
+        _result += "Saudável";
+      else if (imc < 25.0 || imc < 30.0)
+        _result += "Sobrepeso";  
+      else if (imc < 30.5 || imc < 35.0)
+        _result += "Obesidade de Grau I";  
+      else if (imc < 35.0 || imc <40)
+        _result += "ObesidadeGrau II (severa) ";
+      else
+        _result += "Obesidade Grau III (mórbida)";
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pagina Inicial'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            child: TextField(
-              controller: num01Controller,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () => num01Controller.clear(),
-                  icon: Icon(Icons.clear),
-                ),
-                border: OutlineInputBorder(),
-                labelText: 'informe o primeiro número',
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            child: TextField(
-              controller: num02Controller,
-              decoration: InputDecoration(
-                suffixIcon: IconButton(
-                  onPressed: () => num01Controller.clear(),
-                  icon: Icon(Icons.clear),
-                ),
-                border: OutlineInputBorder(),
-                labelText: 'informe o segundo número',
-              ),
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print(somar());
-              setState(somar);
-            },
-            child: Text(
-              '+',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-          ),
-          Text(
-            this.resp,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-            ),
-          )
+        appBar: buildAppBar(),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.all(20.0), child: buildForm()));
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: Text('Calculadora de IMC '),
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.refresh),
+          onPressed: () {
+            resetFields();
+          },
+        )
+      ],
+    );
+  }
+
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildTextFormField(
+              label: "Peso (kg)",
+              error: "Informe o seu peso!",
+              controller: _weightController),
+          buildTextFormField(
+              label: "Altura (CM)",
+              error: "Informe a sua altura!",
+              controller: _heightController),
+          buildTextResult(),
+          buildCalculateButton(),
         ],
       ),
+    );
+  }
+
+  Padding buildCalculateButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: RaisedButton(
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            calculateImc();
+          }
+        },
+        child: Text('Calcular', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Padding buildTextResult() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 36.0),
+      child: Text(
+        _result,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  TextFormField buildTextFormField(
+      {TextEditingController controller, String error, String label}) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: label),
+      controller: controller,
+      validator: (text) {
+        return text.isEmpty ? error : null;
+      },
     );
   }
 }
