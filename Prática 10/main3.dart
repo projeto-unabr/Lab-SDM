c0757e4f7208d9286ea17cb9e6c4f4109a41967f
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-13 17:20:32 -0300

    Calcular Volume

---
+++ Prática 10/main3.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  TextEditingController num01Controller = TextEditingController();
+  TextEditingController num02Controller = TextEditingController();
+  TextEditingController num03Controller = TextEditingController();
+  String resp = "  ";
+
+  somar() {
+    int num01 = int.parse(this.num01Controller.text);
+    int num02 = int.parse(this.num02Controller.text);
+    int num03 = int.parse(this.num02Controller.text);
+    int soma = num01 * num02 * num03;
+    this.resp = 'V= $soma m³';
+    return this.resp;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calcular Volume'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: num01Controller,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () => num01Controller.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'Informe o comprimento',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: num02Controller,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () => num02Controller.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'Informe a largura',
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: TextField(
+              controller: num03Controller,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  onPressed: () => num03Controller.clear(),
+                  icon: Icon(Icons.clear),
+                ),
+                border: OutlineInputBorder(),
+                labelText: 'Informe a altura',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print(somar());
+              setState(somar);
+            },
+            child: Text(
+              'Calcular',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Text(
+            this.resp,
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
