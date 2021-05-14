e5de23d97daca76c2a4b92a8a33346f024a202da
projeto-unabr <80779422+projeto-unabr@users.noreply.github.com>
2021-05-13 21:32:04 -0300

    Atividade 2

---
+++ Prática 8/main2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text('Página Inicial'),
+      ),
+      body: Center(
+          child: Container(
+          width: 400,
+          height: 200,
+          color: Colors.red,
+          child: Center(child: Text('Olá, Mundo!')))),
+      backgroundColor: Colors.blue[900],
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Exemplo de botão',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
